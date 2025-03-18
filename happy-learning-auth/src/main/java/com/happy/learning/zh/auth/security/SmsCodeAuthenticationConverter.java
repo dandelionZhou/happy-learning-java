206325a51f5cd4b7bc158a1e5a40ab6eca35f11c
zhouhao <hao.zhou@sinocare.com>
2025-03-18 16:48:04 +0800

    feat(auth) 从请求头的Authorization中获取client_id

--- happy-learning-auth/src/main/java/com/happy/learning/zh/auth/security/SmsCodeAuthenticationConverter.java
+++ happy-learning-auth/src/main/java/com/happy/learning/zh/auth/security/SmsCodeAuthenticationConverter.java
@@ -9,6 +9,8 @@ import org.springframework.security.web.authentication.AuthenticationConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import java.util.Base64;
+
 @Component
 public class SmsCodeAuthenticationConverter implements AuthenticationConverter {
 
@@ -21,12 +23,26 @@ public class SmsCodeAuthenticationConverter implements AuthenticationConverter {
 
         String phone = request.getParameter("phone");
         String code = request.getParameter("code");
-        String clientId = request.getParameter(OAuth2ParameterNames.CLIENT_ID);
+        String authorization = request.getHeader("Authorization");
+        if (authorization == null || !authorization.startsWith("Basic ")) {
+            throw new IllegalArgumentException("Invalid Authorization header");
+        }
+        // 移除 "Basic " 前缀
+        String base64Credentials = authorization.substring(6).trim();
+
+        // Base64 解码
+        byte[] decodedBytes = Base64.getDecoder().decode(base64Credentials);
+        String credentials = new String(decodedBytes);
+        // 拆分 client_id 和 client_secret
+        String[] parts = credentials.split(":", 2);
+        if (parts.length != 2) {
+            throw new IllegalArgumentException("Invalid client credentials format");
+        }
 
         if (!StringUtils.hasText(phone) || !StringUtils.hasText(code)) {
             throw new OAuth2AuthenticationException(OAuth2ErrorCodes.INVALID_REQUEST);
         }
 
-        return new SmsCodeAuthenticationToken(phone, code, clientId);
+        return new SmsCodeAuthenticationToken(phone, code, parts[0]);
     }
 }
