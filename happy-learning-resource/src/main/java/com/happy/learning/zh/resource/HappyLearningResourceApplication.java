e36d88eaaa18b78f96592dccf671fc9425ea5793
zhouhao <hao.zhou@sinocare.com>
2025-04-03 16:38:39 +0800

    feat(resource) 接入nacos

--- happy-learning-resource/src/main/java/com/happy/learning/zh/resource/HappyLearningResourceApplication.java
+++ happy-learning-resource/src/main/java/com/happy/learning/zh/resource/HappyLearningResourceApplication.java
@@ -2,10 +2,12 @@ package com.happy.learning.zh.resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.happy.learning.zh.resource.feign")
+@EnableDiscoveryClient
 public class HappyLearningResourceApplication {
 
     public static void main(String[] args) {
